6167d37dac3ce10405c42fffc66f3862679a1aeb
Jotxee <jotxee@jotxee.dev>
2022-01-05 10:25:33 +0100

    first component in vue

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>Mensaje: {{ msg }}</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Test',
+    props: {
+        msg: String
+    }
+}
+</script>
+<style scoped>
+    
+</style>
